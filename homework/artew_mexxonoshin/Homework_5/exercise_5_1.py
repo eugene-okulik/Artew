51a774e4fe9adf2b418e4cbcc65b9930918eb353
Artem <a.mekhonoshin@cdek.ru>
2025-01-09 16:00:14 +0500

    Homework_5

---
+++ homework/artew_mexxonoshin/Homework_5/exercise_5_1.py
@@ -0,0 +1,9 @@
+# Данный список
+
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+
+# Распаковка списка в переменные
+
+name, last_name, city, phone, country = person
+
+print(', '.join(person))

f9d82172c9f2b3daa9978bf7fa024decce418ee7
Artem <a.mekhonoshin@cdek.ru>
2025-01-31 16:15:06 +0500

    Homework_10_3

--- homework/artew_mexxonoshin/Homework_10/exersice_10_3.py
+++ homework/artew_mexxonoshin/Homework_10/exersice_10_3.py
@@ -1,3 +1,42 @@
+def operation_select(func):
+    def wrapper(first, second):
+        if second == 0 and (first == second or first > second):
+            return 'Деление на 0 невозможно.'
+
+        if first == second and first < 0:
+            operation = '*'
+        elif first == second:
+            operation = '+'
+        elif first > second:
+            operation = '-'
+        else:  # Это будет случай, когда second > first
+            operation = '/'
+
+        return func(first, second, operation)
+    return wrapper
+
+
+@operation_select
+def calc(first, second, operation):
+    if operation == '/':
+        return first / second
+    elif operation == '+':
+        return first + second
+    elif operation == '-':
+        return first - second
+    elif operation == '*':
+        return first * second
+    else:
+        return 'Неизвестная операция'
+
+
+print(calc(5,3)) # 2
+print(calc(2,2)) # 4
+print(calc(1,0)) # Деление на 0
+print(calc(2,10)) # 0.2
+print(calc(-5,-5)) # 25
+
+
 PRICE_LIST = '''тетрадь 50р
 книга 200р
 ручка 100р

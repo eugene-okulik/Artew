1b944dffb73f304f3fb7ebb871afd5ce512880e0
Artem <a.mekhonoshin@cdek.ru>
2025-01-31 16:24:37 +0500

    HW_10_3_fix2_flake8_Lint

--- homework/artew_mexxonoshin/Homework_10/exersice_10_3.py
+++ homework/artew_mexxonoshin/Homework_10/exersice_10_3.py
@@ -30,10 +30,10 @@ def calc(first, second, operation):
         return 'Неизвестная операция'
 
 
-print(calc(5, 3)) # 2
-print(calc(2, 2)) # 4
-print(calc(1, 0)) # Деление на 0
-print(calc(2, 10)) # 0.2
+print(calc(5, 3))  #  2
+print(calc(2, 2))  #  4
+print(calc(1, 0))  #  Деление на 0
+print(calc(2, 10))  #  0.2
 print(calc(-5, -5)) # 25
 
 
